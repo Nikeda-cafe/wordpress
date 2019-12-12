7f99f333ce24df0056464c516c04325fe49a5c4f
Nikeda <4104cafeaulait@gmail.com>
2019-12-12 21:48:48 +0900

    【3-6】 エディタのcss適用

--- functions.php
+++ functions.php
@@ -6,6 +6,10 @@ function mytheme_setup(){
 
     // レスポンシブ対応
     add_theme_support("responsive-embeds");
+
+    //エディタ用css
+    add_theme_support("editor-styles");
+    add_editor_style("editor-style.css");
 }
 add_action( 'after_setup_theme', 'mytheme_setup');
 function mytheme_enqueue(){
