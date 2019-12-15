291e68c848d106de2d9bee18f8a6fb8595a70e66
Nikeda <4104cafeaulait@gmail.com>
2019-12-15 21:59:15 +0900

    【5-1】ヘッダーフッターをテンプレート化

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <header class="myhead mycontainer">
+        <a href="<?php echo home_url("/"); ?>"><?php bloginfo("name") ?></a> 
+        <p><?php bloginfo("description") ?></p>
+    </header>
--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <header class="myhead mycontainer">
-        <a href="<?php echo home_url("/"); ?>"><?php bloginfo("name") ?></a> 
-        <p><?php bloginfo("description") ?></p>
-    </header>
+<?php get_header(); ?>
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
         <article <?php post_class("mycontainer"); ?>>
             <div class="myposthead">
@@ -25,9 +12,4 @@
             <?php the_post_navigation(); ?>
         </article>
     <?php endwhile; endif; ?>
-    <footer class="myfoot">
-        <?php bloginfo("name") ?>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
