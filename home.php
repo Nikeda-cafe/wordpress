5d5336c3db889a7163f3e1c49d50d032c2407bd2
Nikeda <4104cafeaulait@gmail.com>
2019-12-17 21:36:41 +0900

    5-4カラムレイアウト調整完了

--- home.php
+++ home.php
@@ -4,15 +4,19 @@
             <h1>最新記事</h1>
             <p>RECENT</p>
         </div>
-        <?php if(have_posts()): while(have_posts()): the_post(); ?>
-            <article <?php post_class(); ?>>
-                <?php if(has_post_thumbnail()): ?>
-                    <figure>
-                        <?php the_post_thumbnail(); ?>
-                    </figure>
-                <?php endif; ?>
-                <h2><?php the_title(); ?></h2>
-            </article>
-        <?php endwhile; endif; ?>
+        <div class="mypostlist">
+            <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                <article <?php post_class(); ?>>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php if(has_post_thumbnail()): ?>
+                            <figure>
+                                <?php the_post_thumbnail(); ?>
+                            </figure>
+                        <?php endif; ?>
+                        <h2><?php the_title(); ?></h2>
+                    </a>
+                </article>
+            <?php endwhile; endif; ?>
+        </div>
     </main>
 <?php get_footer(); ?>
