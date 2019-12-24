8536679cde4940f67c92ef84f6fd282085713987
Nikeda <4104cafeaulait@gmail.com>
2019-12-24 20:46:07 +0900

    7-2プロフィール記載

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,11 @@
+<section class="myprofile">
+    <h2>PROFILE</h2>
+    <figure>
+        <img src="<?php echo get_theme_file_uri('profile.jpg'); ?>" alt="profile">
+    </figure>
+    <strong><?php the_author_meta('display_name') ?></strong>
+    <p><?php the_author_meta('description') ?></p>
+</section>
 <?php if (is_active_sidebar("sidebar-1")): ?>
 <?php dynamic_sidebar("sidebar-1"); ?>
 <?php endif; ?>
