b85dbf967d92ee19a256b1f4f0f4a636e39f143d
Nikeda <4104cafeaulait@gmail.com>
2019-12-28 14:00:40 +0900

    7-4アーカイブページ完成

--- archive.php
+++ archive.php
@@ -2,8 +2,12 @@
     <main class="mycontainer">
         <div class="alignwide">
             <div class="myposthead">
-                <h1>最新記事</h1>
-                <p>RECENT</p>
+                <h1><?php the_archive_title(); ?></h1>
+                <?php if(is_category()):?>
+                        <p>CATEGORY ARCHIVE</p>
+                <?php elseif(is_month()):?>
+                        <p>MONTHLY ARCHIVE</p>
+                <?php endif; ?>
             </div>
             <div class="mypostlist">
                 <?php if(have_posts()): while(have_posts()): the_post(); ?>
