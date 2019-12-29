a6cd2c0fc5cd7ddf999f910eb05638ba45bbd1f1
Nikeda <4104cafeaulait@gmail.com>
2019-12-29 20:09:08 +0900

    9-1追加ファイル

---
+++ single.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <article <?php post_class("mycontainer"); ?>>
+            <div class="myposthead">
+                <?php the_category(); ?>
+                <h1><?php the_title(); ?></h1>
+                <time datetime="<?php echo get_the_date(DATE_W3C) ?>">
+                    <?php $date = new DateTime("2019-12-14"); echo $date -> format("Y年m月d日"); ?>
+                </time>
+            </div>
+            <?php the_content(); ?>
+            <?php the_post_navigation(); ?>
+            <aside class="myrelated">
+                <h2>RELATED</h2>
+                <div class="mypostlist">
+                    <?php $posts = get_posts(array(
+                        "posts_per_page" => "4",
+                        "post__not_in" => array(get_the_ID()),
+                        "category__in" => wp_get_post_categories( get_the_ID()),
+                        "orderby" => "rand"
+                    ));
+                        if($posts){
+                            foreach($posts as $post){
+                                setup_postdata( $post ) ?>
+                                <article>
+                                    <a href="<?php the_permalink(); ?>">
+                                    <?php if(has_post_thumbnail()){ ?>
+                                        <figure>
+                                            <?php the_post_thumbnail(); ?>
+                                        </figure>
+                                    <?php  }?> 
+                                        <h2><?php the_title(); ?></h2>
+                                    </a>
+                                </article>
+                    <?php   }
+                        }
+                    ?>
+                </div>
+            </aside>
+        </article>
+    <?php endwhile; endif; ?>
+    <?php get_sidebar(); ?>
+<?php get_footer(); ?>
